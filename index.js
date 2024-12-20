d9237d36ee047f47c2ee831497b87a73fe494ccd
Nigora97 <nigoraaaaa97@gmail.com>
2024-12-20 12:05:46 +0500

    второй таймер

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-function correctDate (date){
+function correctDate(date) {
   return date < 10 ? "0" + date : date;
 }
-
-
 
 function saleDate() {
   const day = document.getElementsByClassName("time")[0];
@@ -14,13 +12,43 @@ function saleDate() {
   const date = finishDate - currentDate;
   console.log(Math.trunc);
   day.innerHTML = correctDate(Math.trunc(date / (1000 * 60 * 60 * 24)));
-  hour.innerHTML = correctDate(Math.trunc(
-    (date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  ));
-  minuts.innerHTML =correctDate(Math.trunc((date % (1000 * 60 * 60)) / (1000 * 60)));
-  seconds.innerHTML =correctDate(Math.trunc((date % (1000 * 60 )) / (1000)));
+  hour.innerHTML = correctDate(
+    Math.trunc((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  minuts.innerHTML = correctDate(
+    Math.trunc((date % (1000 * 60 * 60)) / (1000 * 60))
+  );
+  seconds.innerHTML = correctDate(Math.trunc((date % (1000 * 60)) / 1000));
 }
 saleDate();
-setInterval(function(){
+setInterval(function () {
   saleDate();
-},1000)
+}, 1000);
+
+happyDate();
+function happycorrectDate(date) {
+  return date < 10 ? "0" + date : date;
+}
+
+function happyDate() {
+  const day = document.getElementsByClassName("happytime")[0];
+  const hour = document.getElementsByClassName("happytime")[1];
+  const minuts = document.getElementsByClassName("happytime")[2];
+  const seconds = document.getElementsByClassName("happytime")[3];
+  const finishDate = new Date(2024, 11, 31);
+  const currentDate = new Date();
+  const date = finishDate - currentDate;
+  console.log(Math.trunc);
+  day.innerHTML = happycorrectDate(Math.trunc(date / (1000 * 60 * 60 * 24)));
+  hour.innerHTML = happycorrectDate(
+    Math.trunc((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  minuts.innerHTML = happycorrectDate(
+    Math.trunc((date % (1000 * 60 * 60)) / (1000 * 60))
+  );
+  seconds.innerHTML = happycorrectDate(Math.trunc((date % (1000 * 60)) / 1000));
+}
+happyDate();
+setInterval(function () {
+  happyDate();
+}, 1000);
